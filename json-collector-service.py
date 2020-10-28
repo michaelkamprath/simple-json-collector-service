1a7586558420fd7b87f5ea02143356130b3858ad
Michael Kamprath <michael@kamprath.net>
2020-10-28 14:46:06 -0700

    added more robust web server
    Moved the web server from the default Bottle single-threaded backend to using cherrypy to mitigate the occasional lock ups experienced when server is under load. Also generally improved error handling.

--- json-collector-service.py
+++ json-collector-service.py
@@ -1,29 +1,57 @@
 import json
 import time
-from bottle import route, get, post, run, request, static_file
+from bottle import Bottle, route, get, post, run, request, static_file, response
+
+try:
+    from cheroot.wsgi import Server as WSGIServer
+except ImportError:
+    from cherrypy.wsgiserver import CherryPyWSGIServer as WSGIServer
 
 DATA_FILE_DIR = '/run/collector'
 DATE_FILE_EXTENSION = 'jsonl'
+LOG_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+app = Bottle()
 
 def clean_project_name(project):
     # since project is used a a file name, remove any symbols 
     return "".join(filter(str.isalnum, project))
-         
-@get('/json-collector/<project>')
+
+def logRequestEvent(time_str, json_data):
+    print('{0} [{1}] {2} {3} {4} {5}'.format(
+            request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR'),
+            time_str,
+            request.method,
+            request.url,
+            response.status,
+            '-' if json_data is None else json_data
+        ))
+        
+@app.get('/json-collector/<project>')
 def return_json_data(project):
     cleaned_project = clean_project_name(project)
-    print('Downloading JSON data file for \'{0}\''.format(cleaned_project))
+    logRequestEvent(time.strftime(LOG_TIME_FORMAT), None)
     return static_file('{0}.{1}'.format(cleaned_project, DATE_FILE_EXTENSION), root=DATA_FILE_DIR)
         
 
-@post('/json-collector/<project>')
+@app.post('/json-collector/<project>')
 def ingest_json_data(project):
+    event_time = time.time()
+    event_time_str = time.strftime(LOG_TIME_FORMAT, time.gmtime(event_time))
     cleaned_project = clean_project_name(project)
-    json_data = request.json
+    try:
+        json_data = request.json
+    except json.decoder.JSONDecodeError:
+        response.status = 400
+        logRequestEvent(
+            event_time_str,
+            request.body.getvalue().decode("utf-8").replace('\n','').replace('\t',' ')
+        )
+        return "ERROR - improperly formatted JSON data"
     if json_data is None:
         json_data = ''
     data_dict = {
-        'timestamp': time.time(),
+        'timestamp': event_time,
         'client_ip': request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR'),
         'request_headers':{}, 
         'request_url': request.url,
@@ -35,11 +63,31 @@ def ingest_json_data(project):
     filename = '{0}/{1}.{2}'.format(DATA_FILE_DIR, cleaned_project, DATE_FILE_EXTENSION)
     with open(filename, 'a') as f:
         f.write(json.dumps(data_dict)+'\n')
-    print('Received JSON data for \'{0}\': {1}'.format(cleaned_project, data_dict))
-    return 'JSON data accepted for {0}\n'.format(cleaned_project)
+        
+    logRequestEvent(event_time_str, json_data)
+    return 'JSON data accepted for {0}'.format(project)
 
-@get('/json-collector/health-check')
+@app.get('/json-collector/health-check')
 def return_health_check():
-	return 'Everything is ay oh kay\n'
+    logRequestEvent(time.strftime(LOG_TIME_FORMAT), None)
+    return 'Everything is ay oh kay'
 
-run(host='0.0.0.0', port=8000, debug=True)
+@app.error(404)
+def error404(error):
+    logRequestEvent(time.strftime(LOG_TIME_FORMAT), None)
+    return 'Unknown URL'
+
+if __name__ == "__main__":
+    server = WSGIServer(
+        ('0.0.0.0', 8000),
+        app,
+        server_name='simple-json-collector',
+        numthreads=8
+    )
+
+    try:
+        print("Simple JSON Collector Service starting:\thttp://0.0.0.0:8000/")
+        server.start()
+    except KeyboardInterrupt:
+        print("Halting Simple JSON Collector Service")
+        server.stop()
